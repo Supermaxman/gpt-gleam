1418186c84310e9a8539cc63e5730f7004a711ad
Max Weinzierl <maxwellweinzierl@gmail.com>
2023-12-06 16:45:21 -0600

    Initial commit

--- configs/cfact/experiments.sh
+++ configs/cfact/experiments.sh
@@ -1,11 +1,11 @@
 
-python gpt_gleam/gleam.py \
+python gpt_gleam/labeled.py \
   --config configs/cfact/cfact-gpt4.yaml \
   --data_path /shared/hltdir4/disk1/team/data/corpora/co-vax-frames/covid19/co-vax-frames-train.jsonl \
   --frame_path /shared/hltdir4/disk1/team/data/corpora/co-vax-frames/covid19/frames.json \
   --output_path /shared/aifiles/disk1/media/artifacts/cfact/co-vax-frames/cfact-gpt4.jsonl
 
-python gpt_gleam/gleam.py \
+python gpt_gleam/unlabeled.py \
   --config configs/cfact/cfact-gpt4.yaml \
   --data_path /shared/hltdir4/disk1/team/data/corpora/co-vax-frames/covid19/co-vax-frames-test.jsonl \
   --frame_path /shared/hltdir4/disk1/team/data/corpora/co-vax-frames/covid19/frames.json \
