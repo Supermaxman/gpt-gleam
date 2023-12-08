aa4eeea580cad3c0b7ff6986e8c48897264bdc99
Max Weinzierl <supermaxman1@gmail.com>
2023-12-08 12:56:14 -0600

    Added direct for GPT-3.5

--- configs/cfact/experiments.sh
+++ configs/cfact/experiments.sh
@@ -61,4 +61,18 @@ python gpt_gleam/labeled.py \
   --config configs/cfact/cot-gpt4v.yaml \
   --data_path /shared/aifiles/disk1/media/twitter/v10/data/test.jsonl \
   --frame_path /shared/hltdir4/disk1/team/data/corpora/co-vax-frames/covid19/frames.json \
-  --output_path /shared/aifiles/disk1/media/artifacts/cfact/co-vax-frames/cot-gpt4v.jsonl --debug
+  --output_path /shared/aifiles/disk1/media/artifacts/cfact/co-vax-frames/cot-gpt4v.jsonl
+
+
+python gpt_gleam/eval.py \
+    --data_path /shared/aifiles/disk1/media/twitter/v10/data/test.jsonl \
+    --frame_path /shared/hltdir4/disk1/team/data/corpora/co-vax-frames/covid19/frames.json \
+    --pred_path /shared/aifiles/disk1/media/artifacts/cfact/co-vax-frames/cot-gpt4v.jsonl \
+    --output_path /shared/aifiles/disk1/media/artifacts/cfact/co-vax-frames/results-gpt4v-cot-test.txt
+
+
+python gpt_gleam/labeled.py \
+  --config configs/cfact/direct-gpt3.5.yaml \
+  --data_path /shared/hltdir4/disk1/team/data/corpora/co-vax-frames/covid19/co-vax-frames-test.jsonl \
+  --frame_path /shared/hltdir4/disk1/team/data/corpora/co-vax-frames/covid19/frames.json \
+  --output_path /shared/aifiles/disk1/media/artifacts/cfact/co-vax-frames/direct-gpt3.5.jsonl
