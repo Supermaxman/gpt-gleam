aae5fa0ac5958b3626f26bc9a81c1f90ce74674e
Max Weinzierl <supermaxman1@gmail.com>
2023-12-08 10:55:24 -0600

    Added direct and CoT

--- configs/cfact/experiments.sh
+++ configs/cfact/experiments.sh
@@ -43,3 +43,16 @@ python gpt_gleam/eval.py \
     --frame_path /shared/hltdir4/disk1/team/data/corpora/co-vax-frames/covid19/frames.json \
     --pred_path /shared/aifiles/disk1/media/artifacts/cfact/co-vax-frames/verify-gpt4v-test.jsonl \
     --output_path /shared/aifiles/disk1/media/artifacts/cfact/co-vax-frames/results-gpt4v-test.txt
+
+
+python gpt_gleam/labeled.py \
+  --config configs/cfact/direct-gpt4v.yaml \
+  --data_path /shared/aifiles/disk1/media/twitter/v10/data/test.jsonl \
+  --frame_path /shared/hltdir4/disk1/team/data/corpora/co-vax-frames/covid19/frames.json \
+  --output_path /shared/aifiles/disk1/media/artifacts/cfact/co-vax-frames/direct-gpt4v.jsonl
+
+python gpt_gleam/labeled.py \
+  --config configs/cfact/cot-gpt4v.yaml \
+  --data_path /shared/aifiles/disk1/media/twitter/v10/data/test.jsonl \
+  --frame_path /shared/hltdir4/disk1/team/data/corpora/co-vax-frames/covid19/frames.json \
+  --output_path /shared/aifiles/disk1/media/artifacts/cfact/co-vax-frames/cot-gpt4v.jsonl
