72a7ae3e594fbd3da85431120163e586dc0976c1
Max Weinzierl <supermaxman1@gmail.com>
2023-12-08 12:32:48 -0600

    Added direct and CoT

--- gpt_gleam/results.py
+++ gpt_gleam/results.py
@@ -23,9 +23,9 @@ class TabularResultsWriter:
         for k, v in results.items():
             k = k.replace("_", " ").title()
             if any(k.endswith(f" {m}") for m in self.point_metrics):
-                v = round(v * 100, 1)
+                v = f"{round(v * 100, 1):.1f}"
             else:
-                v = round(v, 3)
+                v = f"{round(v, 3):.3f}"
             f_results[k] = v
         return f_results
 
