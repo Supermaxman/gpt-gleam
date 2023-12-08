296f35827a15c2e12b18d3c59aec0380d20f70c2
Max Weinzierl <supermaxman1@gmail.com>
2023-12-08 12:39:49 -0600

    Added direct and CoT

--- gpt_gleam/results.py
+++ gpt_gleam/results.py
@@ -20,21 +20,24 @@ class TabularResultsWriter:
 
     def _format_values(self, results: dict[str, float]):
         f_results = {}
+        formats = []
         for k, v in results.items():
             k = k.replace("_", " ").title()
             if any(k.endswith(f" {m}") for m in self.point_metrics):
                 v = f"{v * 100:.1f}"
+                formats.append(".1f")
             else:
                 v = f"{v:.3f}"
+                formats.append(".3f")
             f_results[k] = v
-        return f_results
+        return f_results, formats
 
     def write(self, results: dict[str, float]):
-        results = self._format_values(results)
+        results, formats = self._format_values(results)
         print(results)
         df = pd.DataFrame.from_records([results], coerce_float=False)
         print(df)
-        self.file.write(df.to_markdown(index=False, floatfmt=".1f"))
+        self.file.write(df.to_markdown(index=False, floatfmt=formats))
         print(df.to_markdown(index=False, floatfmt=".1f"))
         self.file.flush()
         os.fsync(self.file.fileno())
