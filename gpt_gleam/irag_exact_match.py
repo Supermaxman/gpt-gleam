9350c6ff1aa6cc140b942043f65e15d87c144543
Max Weinzierl <supermaxman1@gmail.com>
2024-08-20 23:19:38 -0500

    added exact match

---
+++ gpt_gleam/irag_exact_match.py
@@ -0,0 +1,80 @@
+import argparse
+
+import ujson as json
+
+from gpt_gleam.data import read_jsonl
+
+
+def key_fn(frame):
+    frame_text = frame["frame"]
+    frame_text = frame_text.replace(" ", "")
+    frame_text = frame_text.replace("\n", "")
+    frame_text = frame_text.replace("\t", "")
+    frame_text = frame_text.lower()
+    frame_text = frame_text.strip()
+    # remove all punctuation
+    frame_text = "".join([c for c in frame_text if c.isalnum()])
+    return frame_text
+
+
+def main(
+    pred_path: str,
+    output_path: str,
+):
+    unique_frames = {}
+    for pred in read_jsonl(pred_path):
+        pred_frames = json.loads(pred["content"])
+        for frame in pred_frames:
+            f_key = key_fn(frame)
+            if f_key not in unique_frames:
+                unique_frames[f_key] = {
+                    "frame": frame["frame"],
+                    "problems": {
+                        p["problem"]: {
+                            "locations": {l["location"]: 1 for l in p["locations"]},
+                            "count": 1,
+                        }
+                        for p in frame["problems"]
+                    },
+                    "count": 1,
+                }
+            else:
+                unique_frames[f_key]["count"] += 1
+                for p in frame["problems"]:
+                    if p["problem"] not in unique_frames[f_key]["problems"]:
+                        unique_frames[f_key]["problems"][p["problem"]] = {
+                            "locations": {l["location"]: 1 for l in p["locations"]},
+                            "count": 1,
+                        }
+                    else:
+                        unique_frames[f_key]["problems"][p["problem"]]["count"] += 1
+                        for l in p["locations"]:
+                            if l["location"] not in unique_frames[f_key]["problems"][p["problem"]]["locations"]:
+                                unique_frames[f_key]["problems"][p["problem"]]["locations"][l["location"]] = 1
+                            else:
+                                unique_frames[f_key]["problems"][p["problem"]]["locations"][l["location"]] += 1
+
+    frames = {
+        f"F{i}": frame
+        for i, (_, frame) in enumerate(
+            sorted(unique_frames.items(), key=lambda x: x[1]["count"], reverse=True), start=1
+        )
+    }
+    print(f"Frames: {len(frames):,}")
+    with open(output_path, "w") as f:
+        json.dump(frames, f, indent=2)
+
+    print(f"Output: {output_path}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, required=True, help="path to config file")
+    parser.add_argument("--pred_path", type=str, required=True, help="path to data jsonl file")
+    parser.add_argument("--output_path", type=str, required=True, help="path to output jsonl file")
+    args = parser.parse_args()
+
+    main(
+        pred_path=args.pred_path,
+        output_path=args.output_path,
+    )
